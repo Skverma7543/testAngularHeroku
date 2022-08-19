c7641c43148ac37bb1d8115aac920eec49c6bcca
shubham <skverma7543@gmail.com>
2022-08-19 22:28:46 +0530

    heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {
+    path:'portfolio-shubham/',
+    component:HomeComponent
+  },
+  {
+    path:'**',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
